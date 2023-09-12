cb8448a7032d2b4745c2d0835b88685a675f4b45
lisandro-kruger <lisandro.kruger@outlook.es>
2023-09-12 10:43:59 -0300

    20230912

---
+++ src/app/editar-producto/editar-producto.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductoService } from '../producto.service';
+
+@Component({
+  selector: 'app-editar-producto',
+  templateUrl: './editar-producto.component.html',
+  styleUrls: ['./editar-producto.component.css']
+})
+export class EditarProductoComponent {
+  producto: Producto = new Producto();
+  id: number;
+
+  constructor(private productoServicio: ProductoService,
+    private ruta: ActivatedRoute, private enrutador: Router){}
+
+  ngOnInit(){
+    this.id = this.ruta.snapshot.params['id'];
+    this.productoServicio.obtenerProductoPorId(this.id).subscribe(
+      {
+        next: (datos) => this.producto = datos,
+        error: (errores: any) => console.log(errores)
+      }
+    );
+  }
+
+  onSubmit(){
+    this.editarProducto();
+  }
+
+  editarProducto(){
+    this.productoServicio.editarProducto(this.id, this.producto).subscribe({
+      next: (datos) => this.irProductoLista(),
+      error: (errores) => console.log(errores)
+    });
+  }
+
+  irProductoLista(){
+    this.enrutador.navigate(['/productos']);
+  }
+}
